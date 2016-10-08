097ccec14509f99f2a8c8c5050b2d95d9e71240a
Ryan Kurte <ryankurte@gmail.com>
2016-10-08 01:12:35 +0000

    Setup script now working:

--- setup.sh
+++ setup.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get dist-upgrade
+# Make sure your system is up to date and install dependencies before running.
+# sudo apt-get update
+# sudo apt-get dist-upgrade
+# sudo apt-get install -yq bc libncurses5-dev
+
+#set -e
 
 echo "Enable kernel I2C/I2S"
 sudo sed -i "s/#dtparam=i2s=on/dtparam=i2s=on/g" /boot/config.txt
 
 echo "Enable I2S kernel modules"
-#TODO: only do this once...
-#echo "snd_soc_bcm2708" | sudo tee -a /etc/modules
-#echo "snd_soc_bcm2708_i2s" | sudo tee -a /etc/modules
-#echo "nbcm2708_dmaengine" | sudo tee -a /etc/modules
+if ! grep -Fxq "snd_soc_bcm2708" /etc/modules; then
+  echo "snd_soc_bcm2708" | sudo tee -a /etc/modules
+fi
+
+if ! grep -Fxq "snd_soc_bcm2708_i2s" /etc/modules; then
+  echo "snd_soc_bcm2708_i2s" | sudo tee -a /etc/modules
+fi
+
+if ! grep -Fxq "snd_soc_bcm2708" /etc/modules; then
+  echo "nbcm2708_dmaengine" | sudo tee -a /etc/modules
+fi
 
 echo "Install kernel sources"
-sudo apt-get install bc libncurses5-dev
-git clone http://github.com/notro/rpi-source
-cd rpi-source && python rpi-source && cd ..
+if [ -d "rpi-source" ]; then
+  git -C rpi-source pull
+else
+  git clone http://github.com/notro/rpi-source
+fi
+
+cd rpi-source
+python rpi-source
+cd ..
+
+pwd
 
 echo "Build modules"
 cd snd_driver && make && cd ..
